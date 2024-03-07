ee3e4a206a5ac51d687ede080fd0e30a6a57feb5
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2024-03-07 14:26:04 +0100

    fix routing

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 import PokemonInfo from "./routes/PokemonInfo/PokemonInfo";
 
 ReactDOM.render(
-  <BrowserRouter basename="/PokedexApp">
+  <BrowserRouter basename="">
     {/*<nav style={{textAlign: "center", fontSize: '50px'}}>POKEDEX</nav>*/}
-    <App/>
-    <Routes key={'Routes'}>
+    <App />
+    <Routes key={"Routes"}>
       {/*<Route key={'main_route'} path="/" element={<App/>}>*/}
-        <Route key={'Route_pokemon'} path="/pokemon" element={<PokemonInfo/>}>
-          <Route key={'Route_pokemon_id'} path=":pokemon" element={<PokemonInfo/>}/>
-        </Route>
+      <Route key={"Route_pokemon"} path="/pokemon" element={<PokemonInfo />}>
+        <Route
+          key={"Route_pokemon_id"}
+          path=":pokemon"
+          element={<PokemonInfo />}
+        />
+      </Route>
       {/*</Route>*/}
     </Routes>
-    <div style={{clear: "both"}}/>
+    <div style={{ clear: "both" }} />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
